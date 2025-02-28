a8104a27d6de2ed6e7356bcd34dabda1a7c9d268
Utkarsha Turambekar <utkarshat200318@gmail.com>
2025-02-28 19:28:13 +0530

    Creating api for all operations

--- backend/Routes/contactRoutes.js
+++ backend/Routes/contactRoutes.js
@@ -1,8 +1,29 @@
 const express=require("express");
 const router=express.Router();
 
-router.get("/",(req,res)=>{
+//get all contacts
+router.route("/").get((req,res)=>{
     res.send("Get all the contacts from diary ");
+});
+
+//get specified contact with given id
+router.route("/:id").get((req,res)=>{
+    res.send(`Get the contact with id ${req.params.id}`)
+});
+
+//create contact with post operation
+router.route("/").post((req,res)=>{
+    res.send(`creating the contact`)
+});
+
+//Update contact with the PUT operation
+router.route("/:id").put((req,res)=>{
+    res.send(`Updating the contact with the given id ${req.params.id}`)
+});
+
+//Deleting the contact with given id
+router.route("/:id").delete((req,res)=>{
+    res.send(`deleting the contact with the ${req.params.id}`)
 });
 
 module.exports=router;
