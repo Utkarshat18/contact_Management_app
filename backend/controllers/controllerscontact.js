778f9e7473008abba0ff11be3eba137e14a188c3
Utkarsha Turambekar <utkarshat200318@gmail.com>
2025-03-04 16:16:20 +0530

    async-express-handler setup

--- backend/controllers/controllerscontact.js
+++ backend/controllers/controllerscontact.js
@@ -1,5 +1,7 @@
+const asyncHandler=require("express-async-handler");    
+
 //to get all the contacts
-const getallcontact=(req,res)=>{
+const getallcontact=asyncHandler(async(req,res)=>{
     console.log("body request :",req.body);
     const {name,age,phonenumber}=req.body;
     if(!name || !age || !phonenumber)
@@ -8,26 +10,26 @@ const getallcontact=(req,res)=>{
         throw new Error ("All fields are mandatory");
     }
     res.json({message:"Get all the contacts"});
-}
+});
 
 //to get one specific contact with the id
-const getidcontact=(req,res)=>{
+const getidcontact=asyncHandler(async(req,res)=>{
     res.json({message:`get the contact with the id ${req.params.id}`})
-}
+})
 
 //to create a new contact
-const createcontact=(req,res)=>{
+const createcontact=asyncHandler(async(req,res)=>{
     res.json({message:`creating a new contact`})
-}
+})
 
 //updating the given contact
-const updatecontact=(req,res)=>{
+const updatecontact=asyncHandler(async(req,res)=>{
     res.json({message:`updating the contact with id ${req.params.id}`})
-}
+})
 
 //deleting the contact
-const deletecontact=(req,res)=>{
+const deletecontact=asyncHandler(async(req,res)=>{
     res.json({message:`deleting the given contact with id ${req.params.id}`})
-}
+})
 
 module.exports={getallcontact,getidcontact,createcontact,updatecontact,deletecontact};
