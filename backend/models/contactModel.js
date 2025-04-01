936a0ae52a158faa9d615f8a36cb6cc7f642223b
Utkarsha Turambekar <utkarshat200318@gmail.com>
2025-04-01 20:03:54 +0530

    database connections

---
+++ backend/models/contactModel.js
@@ -0,0 +1,21 @@
+const mongoose=require("mongoose");
+
+const contactSchema=mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,"Please add the name "],
+    },
+    age:{
+        type:String,
+        required:[true,"Please add the age "],
+    },
+    phonenumber:{
+        type:String,
+        required:[true,"Please add your phone number"],
+    }
+},{
+    timestamps:true,
+})
+
+module.exports=mongoose.model("Contact",contactSchema);
+//45 minutes
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,17 @@
 //creating a basic express server
 const express=require("express");
 const errorHandler = require("./middleware/errorHandler.js");
+const connectDb = require("./config/dbConnection.js");
 const dotenv=require("dotenv").config();
+
+
+connectDb();
 const app=express();
 
 const port=process.env.PORT || 6000;
-
 // app.get("/api/contacts",(req,res)=>{
 //     res.send("Get all the contacts");
 // });
-
 app.use(express.json());//work as a middleware to accept the request body from server
 app.use("/api/contacts",require('./Routes/contactRoutes.js'));
 app.use(errorHandler);
